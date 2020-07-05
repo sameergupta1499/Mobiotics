52959c1da42bdcf396f4771fe0ab83370407ac30
sameergupta1499 <sameergupta1499@gmail.com>
2020-07-05 13:29:15 +0530

    Updates

---
+++ precache-manifest.c082064a42e2a1fded8da7d773edaae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92984d8af8f1b90a4ee513c0133b21f7",
+    "url": "/Mobiotics/index.html"
+  },
+  {
+    "revision": "7ac42f9815bb21d08da3",
+    "url": "/Mobiotics/static/css/main.bf523082.chunk.css"
+  },
+  {
+    "revision": "8f9d50648005f573bf86",
+    "url": "/Mobiotics/static/js/2.2c409a14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mobiotics/static/js/2.2c409a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac42f9815bb21d08da3",
+    "url": "/Mobiotics/static/js/main.fe25e9ac.chunk.js"
+  },
+  {
+    "revision": "e94f3e0514653dddb2df",
+    "url": "/Mobiotics/static/js/runtime-main.cbdb0724.js"
+  },
+  {
+    "revision": "7bb021bf29a86a7c59661680825c354d",
+    "url": "/Mobiotics/static/media/greenTick.7bb021bf.gif"
+  }
+]);
